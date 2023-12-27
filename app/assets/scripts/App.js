eac19655150ff54e6870d890f6f2381d5c8c6115
Mamba <khayratbekt@gmail.com>
2023-12-27 17:59:35 +0800

    modal overlay-g buren hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,29 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import Modal from "./modules/Modal";
 import RevealOnscroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/stickyHeader";
 
 let stickyHeader = new StickyHeader();
 new RevealOnscroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnscroll(document.querySelectorAll(".testimonial"), 50);
-
 let mobileMenu = new MobileMenu();
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal"*/ "./modules/Modal").then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openModal(), 20)
+            })
+                .catch(() => console.log("Failig duudahad aldaa garlaa"));
+        } else {
+            modal.openModal();
+        }
+    })
+});
+
 if (module.hot) {
     module.hot.accept();
 }
