c3004106b327a8bb9905b31b0532ec5c3857f243
Mamba <khayratbekt@gmail.com>
2023-12-27 15:17:10 +0800

    Reveal feature-iig buren hiij duusgav

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnscroll from "./modules/RevealOnScroll";
+new RevealOnscroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnscroll(document.querySelectorAll(".testimonial"), 50);
 
 let mobileMenu = new MobileMenu();
 if (module.hot) {
