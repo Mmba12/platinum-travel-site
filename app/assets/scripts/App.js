8acb9de6e9420e5bc1d1c137c25493a804879131
Mamba <khayratbekt@gmail.com>
2023-12-27 18:18:02 +0800

    lazy loading buren hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import "../styles/styles.css";
+import "lazysizes";
 import MobileMenu from "./modules/MobileMenu";
 import Modal from "./modules/Modal";
 import RevealOnscroll from "./modules/RevealOnScroll";
